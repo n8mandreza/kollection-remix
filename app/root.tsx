8b182252b610159fa6303b10388d9d391d2b1ede
Nathan Mandreza <n8@nathanmandreza.com>
2023-01-27 01:00:45 -0800

    Initial commit

---
+++ app/root.tsx
@@ -0,0 +1,40 @@
+import type { MetaFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import banner from "~/assets/TheKollection_WEB_LP_cover.png";
+import styles from "~/styles/app.css"
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }]
+}
+
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "The Kollection",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <div className='w-screen h-screen overflow-hidden flex justify-center items-center'>
+          <img className='w-full h-full' src={banner} alt="The Kollection" />
+        </div>
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
